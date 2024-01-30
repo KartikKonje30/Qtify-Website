200ebe34f05202ee65d3fd2a89f12e80c0a57015
KartikKonje30 <kartikkonje.01@gmail.com>
2024-01-30 18:42:56 +0530

    3rd commit

--- qtify/src/App.js
+++ qtify/src/App.js
@@ -1,7 +1,14 @@
 import Navbar from "./components/Navbar/Navbar";
 import Hero from './components/Hero/Hero'
+import { useState } from "react";
 
 function App() {
+  const [data,setData] = useState([]);
+
+  const generateTopAlbums = () => {
+
+  };
+
   return (
     <div >
       <Navbar />
