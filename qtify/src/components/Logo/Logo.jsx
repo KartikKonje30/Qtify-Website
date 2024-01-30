d633fdeae7252d7cd42020a18625914fa63e5a6b
KartikKonje30 <kartikkonje.01@gmail.com>
2024-01-30 17:14:07 +0530

    1st commit

--- qtify/src/components/Logo/Logo.jsx
+++ qtify/src/components/Logo/Logo.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import LogoImage from '../../assets/Group 3748.png'
-import styles from './Logo.module.css'
 
 const Logo = () => {
   return (
-    <img className={styles.logo} src={LogoImage} alt='Logo' />
+    <img src={LogoImage} alt='Logo' width="67" height="34"/>
   )
 }
 
