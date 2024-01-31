a2b902cb3bda73a61d0b15c4ce67e7566500c0b6
KartikKonje30 <kartikkonje.01@gmail.com>
2024-01-31 20:32:11 +0530

    5th commit

--- qtify/src/components/Section/Section.jsx
+++ qtify/src/components/Section/Section.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import styles from './Section.module.css';
-import { Box, CircularProgress } from '@mui/material';
+import { CircularProgress } from '@mui/material';
 import Card from '../Card/Card';
 import Carousel from '../Carousel/Carousel';
 import BasicTabs from '../Tabs/Tabs';
 
-const Section = ({data,title,type, filteredData=null, filteredDataValues=[], toggle = false, handleToggle = null, value = 0, handleChange=null}) => {
+const Section = ({data,title,type, filteredDataValues=[], toggle = false, handleToggle = null, value = 0, handleChange=null}) => {
 
   return (
     <div>
@@ -20,17 +20,17 @@ const Section = ({data,title,type, filteredData=null, filteredDataValues=[], tog
         ) : null}
         {
             data.length === 0 ? (
-                <Box sx={{display: "flex", justifyContent: "center", alignItems: "center" }}> 
+                // <Box sx={{display: "flex", justifyContent: "center", alignItems: "center" }}> 
                     <CircularProgress />
-                </Box>
+                // </Box>
             ):(
                 <div className={styles.cardsWrapper}>
-                    {toggle ? (
+                    {!toggle ? (
                     <div className={styles.wrapper}>
                         {
-                            filteredDataValues.map((item) => {
-                                return <Card data={item} type={type}/>
-                            })}
+                            filteredDataValues.map((item) => (
+                                <Card data={item} type={type}/>
+                            ))}
                     </div>
                     ) : ( <Carousel data={filteredDataValues} renderComponent={(data) => <Card data={data} type={type} />} />)}
                 </div>
