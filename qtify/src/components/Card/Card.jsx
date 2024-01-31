59cca79bdc6e905d77eb321f4fade2a0ce3cebec
KartikKonje30 <kartikkonje.01@gmail.com>
2024-01-31 21:19:00 +0530

    8th commit

--- qtify/src/components/Card/Card.jsx
+++ qtify/src/components/Card/Card.jsx
@@ -26,25 +26,6 @@ const Card = ({data, type}) => {
                 );
             }
 
-            case "new":{
-                // eslint-disable-next-line no-unused-vars
-                const {image, follows, title, slug, songs} = data;
-                return (
-                    <Tooltip title={`${songs.length} songs`} placement='top' arrow>
-                        <div className={styles.wrapper}>
-                            <div className={styles.card}>
-                                <img src={image} alt='Album'/>
-                                <div className={styles.banner}>
-                                    <Chip label={`${follows} Follows`} size='small' className={styles.chip}/>
-                                </div>
-                            </div>
-                            <div className={styles.titleWrapper}>
-                            <p>{title}</p>
-                            </div>
-                        </div>
-                    </Tooltip>
-                );
-            }
 
 
             case "song": {
